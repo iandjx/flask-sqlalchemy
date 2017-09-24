e632920f7d9838c56edb1b2316789672b945448b
Ian de Jesus <idejesus@zendesk.com>
2017-09-24 11:19:40 +0900

    88. Creating a new model: StoreModel Create storemodel class Update itemmodel to set relationship with storeModel Update item resource to accept store_id

--- models/storemodel.py
+++ models/storemodel.py
@@ -1,21 +1,18 @@
 from db import db
 
 
-class ItemModel(db.Model):
+class StoreModel(db.Model):
     __tablename__ = 'items'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    price = db.Column(db.Float(precision=2))
-    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
-    store = db.relationship('StoreModel')
 
-    def __init__(self, name, price, store_id):
+    items = db.relationship('ItemModel', lazy ='dynamic') #dynamic used to retrieve all items only when json() is called
+
+    def __init__(self, name):
         self.name = name
-        self.price = price
-        self.store_id
 
     def json(self):
-        return {'name': self.name, 'price': self.price}
+        return {'name': self.name, 'items': [item.json() for item in self.items.all]}
 
     def save_to_db(self):
         db.session.add(self)
--- resources/item.py
+++ resources/item.py
@@ -11,6 +11,12 @@ class Item(Resource):
                         help="This field cannot be left blank"
                         )
 
+    parser.add_argument('store_id',
+                        type=int,
+                        required=True,
+                        help="Every item needs a store id."
+                        )
+
     @jwt_required()
     def get(self, name):
 
@@ -26,7 +32,7 @@ class Item(Resource):
             return {'messsage': "An item with name '{}' already exists.".format(name)}, 400  # 400 is bad request
 
         data = Item.parser.parse_args()
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
 
         try:
             item.save_to_db()
@@ -49,7 +55,7 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
 
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, **data)
         else:
             item.price = data['price']
 
