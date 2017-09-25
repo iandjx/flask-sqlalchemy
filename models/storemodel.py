f5b27cd8eedbdeb264f3882b7bbd1632c70b3b43
Ian de Jesus <idejesus@zendesk.com>
2017-09-25 13:38:04 +0900

    Bug Fixes Initiate self.store_id with store_id filter by to filter_by

--- models/storemodel.py
+++ models/storemodel.py
@@ -1,26 +1,26 @@
 from db import db
 
-
 class StoreModel(db.Model):
-    __tablename__ = 'items'
+    __tablename__ = 'stores'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
 
-    items = db.relationship('ItemModel', lazy ='dynamic') #dynamic used to retrieve all items only when json() is called
+    items = db.relationship('ItemModel', lazy='dynamic')
 
     def __init__(self, name):
         self.name = name
 
     def json(self):
-        return {'name': self.name, 'items': [item.json() for item in self.items.all]}
+        return {'name': self.name, 'items': [item.json() for item in self.items.all()]}
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
 
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
-
-    @classmethod
-    def find_by_name(cls, name):
-        return cls.query.filter_by(name=name).first()  # SELECT * FROM items WHERE name=name LIMIT 1
 
     def delete_from_db(self):
         db.session.delete(self)
